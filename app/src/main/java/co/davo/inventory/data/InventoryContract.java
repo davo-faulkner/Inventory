a1594a1c7bbc0838c012e4487990dd3002ce9d6f
Davo Faulkner <d@davo.co>
2017-09-15 17:19:00 -0500

    Add empty constructor and String constants to InventoryContract.java

--- app/src/main/java/co/davo/inventory/data/InventoryContract.java
+++ app/src/main/java/co/davo/inventory/data/InventoryContract.java
@@ -1,9 +1,38 @@
 package co.davo.inventory.data;
 
+import android.net.Uri;
+
 /**
  * Created by Davo on 8/30/2017.
  */
 
 public final class InventoryContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private InventoryContract() {
+    }
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "co.davo.inventory";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_INVENTORY = "inventory";
 
 }
