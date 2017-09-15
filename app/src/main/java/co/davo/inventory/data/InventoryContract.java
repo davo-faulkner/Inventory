262827fdf7cc090bedb359ea160ea62ede3d8ffe
Davo Faulkner <d@davo.co>
2017-09-15 17:24:39 -0500

    Add InventoryEntry subclass to InventoryContract.java

--- app/src/main/java/co/davo/inventory/data/InventoryContract.java
+++ app/src/main/java/co/davo/inventory/data/InventoryContract.java
@@ -1,6 +1,8 @@
 package co.davo.inventory.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Created by Davo on 8/30/2017.
@@ -35,4 +37,63 @@ public final class InventoryContract {
      */
     public static final String PATH_INVENTORY = "inventory";
 
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class InventoryEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         * Name of database table for pets
+         */
+        public final static String TABLE_NAME = "inventory";
+
+        /**
+         * Unique ID number for the pet (only for use in the database table).
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the pet.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_ITEM_NAME = "name";
+
+        /**
+         * Breed of the pet.
+         * <p>
+         * Type: TEXT
+         */
+        public final static String COLUMN_ITEM_QUANTITY = "quantity";
+
+        /**
+         * Gender of the pet.
+         * <p>
+         * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
+         * or {@link #GENDER_FEMALE}.
+         * <p>
+         * Type: INTEGER
+         */
+        public final static String COLUMN_ITEM_PRICE = "price";
+    }
 }
