48c6666bb8423e727ab1c0f13f7e0654898f511a
Davo Faulkner <d@davo.co>
2017-09-17 18:32:02 -0500

    Finish coding InventoryDbHelper.java

--- app/src/main/java/co/davo/inventory/data/InventoryDbHelper.java
+++ app/src/main/java/co/davo/inventory/data/InventoryDbHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import static android.R.attr.version;
+import co.davo.inventory.data.InventoryContract.InventoryEntry;
 
 /**
  * Created by Davo on 8/30/2017.
@@ -19,7 +20,14 @@ public class InventoryDbHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_INVENTORY_TABLE =  "CREATE TABLE " + InventoryEntry.TABLE_NAME + " ("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InventoryEntry.COLUMN_ITEM_NAME + " TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_ITEM_QUANTITY + " INTEGER NOT NULL DEFAULT 0, "
+                + InventoryEntry.COLUMN_ITEM_PRICE + " INTEGER NOT NULL);";
+
+        db.execSQL(SQL_CREATE_INVENTORY_TABLE);
 
     }
     @Override
