d73875927745a86baaad376d648c65084c0795fa
Davo Faulkner <d@davo.co>
2017-09-17 19:12:59 -0500

    Begin coding InventoryProvider.java

--- app/src/main/java/co/davo/inventory/data/InventoryProvider.java
+++ app/src/main/java/co/davo/inventory/data/InventoryProvider.java
@@ -1,11 +1,15 @@
 package co.davo.inventory.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import co.davo.inventory.data.InventoryContract.InventoryEntry;
 
 /**
  * Created by Davo on 8/30/2017.
@@ -13,21 +17,69 @@ import android.support.annotation.Nullable;
 
 public class InventoryProvider extends ContentProvider {
 
+    public static final String LOG_TAG = InventoryProvider.class.getSimpleName();
+
+    private static final int ITEMS = 100;
+
+    private static final int ITEM_ID = 101;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY,
+                ITEMS);
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY
+                + "/#", ITEM_ID);
+    }
+
+    private InventoryDbHelper mDbHelper;
+
     @Override
     public boolean onCreate() {
-        return false;
+        mDbHelper = new InventoryDbHelper(getContext());
+        return true;
     }
-    @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case ITEMS:
+                cursor = database.query(InventoryEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            case ITEM_ID:
+                selection = InventoryEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                cursor = database.query(InventoryEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
-    @Nullable
     @Override
-    public String getType(@NonNull Uri uri) {
-        return null;
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case ITEMS:
+                return InventoryEntry.CONTENT_LIST_TYPE;
+            case ITEM_ID:
+                return InventoryEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
-    @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
         return null;
