444e17192f1442462f508d6bcb31a351dc983090
Davo Faulkner <d@davo.co>
2017-09-18 23:12:04 -0500

    Finish RecyclerAdapter.java (except Sale Button) and add EditorActivity.java

---
+++ app/src/main/java/co/davo/inventory/EditorActivity.java
@@ -0,0 +1,9 @@
+package co.davo.inventory;
+
+/**
+ * Created by Davo on 9/18/2017.
+ */
+
+public class EditorActivity {
+
+}
--- app/src/main/java/co/davo/inventory/RecyclerAdapter.java
+++ app/src/main/java/co/davo/inventory/RecyclerAdapter.java
@@ -1,13 +1,19 @@
 package co.davo.inventory;
 
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import co.davo.inventory.data.InventoryContract.InventoryEntry;
 
 /**
  * Created by Davo on 9/18/2017.
@@ -21,15 +27,25 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemHo
     }
     @Override
     public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View inflatedView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view_layout, parent, false);
+        return new ItemHolder(inflatedView);
     }
     @Override
     public void onBindViewHolder(ItemHolder holder, int position) {
+        items.moveToPosition(position);
 
+        int nameColumnIndex = items.getColumnIndex(InventoryEntry.COLUMN_ITEM_NAME);
+        int priceColumnIndex = items.getColumnIndex(InventoryEntry.COLUMN_ITEM_PRICE);
+        int quantityColumnIndex = items.getColumnIndex(InventoryEntry.COLUMN_ITEM_QUANTITY);
+
+        holder.nameTextView.setText(items.getString(nameColumnIndex));
+        holder.priceTextView.setText(items.getString(priceColumnIndex));
+        holder.quantityTextView.setText(items.getString(quantityColumnIndex));
     }
     @Override
     public int getItemCount() {
-        return 0;
+        return items.getCount();
     }
 
     public static class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -53,6 +69,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemHo
             Log.d("RecyclerView", "CLICK!");
             Context context = view.getContext();
             int itemPosition = getAdapterPosition();
+            Intent intent = new Intent(context, EditorActivity.class);
+            Uri currentItemUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI,
+                    itemPosition);
+            intent.setData(currentItemUri);
+            context.startActivity(intent);
         }
     }
 
