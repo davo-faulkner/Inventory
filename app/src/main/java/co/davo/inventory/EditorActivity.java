fdaca83b7402e44abbc864e32994be5cf5be33f3
Davo Faulkner <d@davo.co>
2017-09-18 23:27:18 -0500

    Begin coding EditorActivity.java

--- app/src/main/java/co/davo/inventory/EditorActivity.java
+++ app/src/main/java/co/davo/inventory/EditorActivity.java
@@ -1,9 +1,48 @@
 package co.davo.inventory;
 
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import co.davo.inventory.data.InventoryContract.InventoryEntry;
+
 /**
  * Created by Davo on 9/18/2017.
  */
 
-public class EditorActivity {
+public class EditorActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int EXISTING_ITEM_LOADER = 0;
+    private Uri currentItemUri;
+    private EditText nameEditText;
+    private EditText priceEditText;
+    private EditText quantityEditText;
+    private boolean itemHasChanged = false;
 
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            itemHasChanged = true;
+            return false;
+        }
+    };
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
