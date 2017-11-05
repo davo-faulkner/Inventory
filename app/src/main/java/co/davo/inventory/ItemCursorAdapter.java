4e19af78c53f3b60b842e0081a0af10d9ea267a4
Davo Faulkner <d@davo.co>
2017-11-05 03:49:13 -0600

    Add import to ItemCursorAdapter.java and blank list_item.xml

--- app/src/main/java/co/davo/inventory/ItemCursorAdapter.java
+++ app/src/main/java/co/davo/inventory/ItemCursorAdapter.java
@@ -2,9 +2,12 @@ package co.davo.inventory;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+
+import co.davo.inventory.data.InventoryContract.InventoryEntry;
 
 /**
  * Created by Davo on 11/5/2017.
@@ -17,7 +20,7 @@ public class ItemCursorAdapter extends CursorAdapter {
     }
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item,parent, false);
     }
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
