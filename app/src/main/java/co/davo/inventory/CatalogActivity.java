c1202174e1931dbbe5e10371b851c8f1914fb7d4
Davo Faulkner <d@davo.co>
2018-03-20 05:34:43 -0500

    Delete Menu from CatalogActivity.java

--- app/src/main/java/co/davo/inventory/CatalogActivity.java
+++ app/src/main/java/co/davo/inventory/CatalogActivity.java
@@ -71,28 +71,6 @@ public class CatalogActivity extends AppCompatActivity implements
         getLoaderManager().initLoader(ITEM_LOADER, null, this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_catalog, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         String[] projection = {
